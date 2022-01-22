b181a77b905c1ebc6f3af50ee0d9c9d34f458e73
Montana Mendy <montana@linux.com>
2022-01-21 21:35:43 -0800

    coded out on vim, pushed thru vim

---
+++ ratelimit.py
@@ -0,0 +1,18 @@
+def RateLimit(maxPerSecond):
+    minInterval = 1.0 / float(maxPerSecond)
+
+    def decorate(func):
+        lastTimeCalled = [0.0]
+
+        def rateLimitFunction(*args, **kwargs):
+            elapsed = time.clock() - lastTimeCalled[0]
+            leftToWait = minInterval - elapsed
+            if leftToWait > 0:
+                time.sleep(leftToWait)
+            ret = func(*args, **kwargs)
+            lastTimeCalled[0] = time.clock()
+            return ret
+
+        return rateLimitFunction
+
+    return decorate
